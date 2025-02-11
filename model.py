8099a18cd2d6ca19d3385b77bd8ae5c6b06d6290
mete <148999436+metetolga@users.noreply.github.com>
2025-02-11 20:06:09 +0300

    model update

--- model.py
+++ model.py
@@ -13,15 +13,25 @@ class InputEmbedding(nn.Module):
         return self.embedding(x) * math.sqrt(self.d_model)
 
 class PosEncoding(nn.Module):
-    def __init__(self, seq_len:int, d_model:int, dropout_rate:float):
+    def __init__(self, seq_len:int, d_model:int, dropout_rate:float, n:float=10000.0):
         super().__init__()
         self.d_model = d_model
         self.seq_len = seq_len 
         self.dropout = nn.Dropout(p=dropout_rate)
         
         enc = torch.zeros(seq_len, d_model)
-        # TO BE CONTINUED
+        position = torch.arange(0, seq_len, dtype=float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(n)) / d_model)
+        enc[:, 0::2] = torch.sin(position * div_term)
+        enc[:, 1::2] = torch.cos(position * div_term)
+        enc = enc.unsqueeze(0)
+        self.register_buffer('enc', enc)
+    
+    def forward(self, x):
+        # add the positional encoding till the length of the sequence(=x->(batch,seq_len,d_model))
+        # also register buffer ensure its not included in computation graph. 
+        x = x + self.enc[:, :x.shape[1], :]
+        return self.dropout(x) 
 
 if __name__ == '__main__':
-    mbed = InputEmbedding(64, 512)
-    mbed()
+    pass
